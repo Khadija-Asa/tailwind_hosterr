5222e731b13ce386c4ab1b0fa8b12b009cad4502
Khadidja <khadija.aitsiali@gmail.com>
2022-04-15 18:01:27 +0200

    header for mobile done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      rotate: {
+        '20': '15deg',
+        '30': '25deg',
+        '40': '35deg',
+      }
+    },
+  },
+  plugins: [],
+}
